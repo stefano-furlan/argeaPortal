84ec8922f54123db918e69fd32559fb897617709
stefano.furlan <stefano.furlan@botter.it>
2022-11-14 13:52:48 +0100

    esempi di post

--- src/main/java/com/argea/argeaportal/database/ClienteArgeaRepository.java
+++ src/main/java/com/argea/argeaportal/database/ClienteArgeaRepository.java
@@ -1,9 +1,36 @@
 package com.argea.argeaportal.database;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface ClienteArgeaRepository extends CrudRepository<ClienteArgea, Integer> {
 
+    /**
+     * esempio di metodo in cui la libreria inferisce dal nome metodo la query(intellisense funziona)
+     * @param descrizione
+     * @return lista clienti argea che matchano
+     */
+    List<ClienteArgea> findAllByDescrizioneLikeIgnoreCase(String descrizione);
 
+    /***
+     * esempio di query HQL, i nome campo sono i nomi dei campi della classe entity (e non del db) e garantisce database independence
+     * @param idMaggioreDi
+     * @return
+     */
+    @Query("SELECT c FROM ClienteArgea c " +
+            " WHERE c.descrizione ='pippo' and c.id>?1 " )
+    List<ClienteArgea> findClientiCheSiChiamanoPippoConIdMaggioreDi(Integer idMaggioreDi);
 
+    /***
+     * esempio di query nativa, i nome campo sono i nomi dei campi del db e NON garantisce database independence
+     * DA EVITARE
+     * @param idMaggioreDi
+     * @return
+     */
+   /* @Query(value = "SELECT * FROM ClienteArgea c " +
+            " WHERE c.descrizione ='pippo' and c.id>?1 " ,nativeQuery = true)
+    List<ClienteArgea> findClientiCheSiChiamanoPippoConIdMaggioreDiConQueryNativaDAEVITARE(Integer idMaggioreDi);
+*/
 }
---
+++ src/main/java/com/argea/argeaportal/rest/RestMasterDataController.java
@@ -0,0 +1,31 @@
+package com.argea.argeaportal.rest;
+
+import com.argea.argeaportal.database.ClienteArgea;
+import com.argea.argeaportal.database.ClienteArgeaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rs/md")
+public class RestMasterDataController {
+
+    @Autowired
+    ClienteArgeaRepository clienteArgeaRepository;
+
+    @GetMapping("/ping")
+    String ping() {
+        return "ping RestMasterDataController";
+    }
+
+
+    @PostMapping("/cliente-argea/save")
+    ClienteArgea saveClienteArgea(@RequestBody ClienteArgea clienteArgea) {
+
+        //TODO: aggiungere eventuali controlli di integrità
+
+        return clienteArgeaRepository.save(clienteArgea);
+
+    }
+
+
+}
